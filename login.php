03eb11c3c7c0ed5eb0f1e8e74b11321b3c7a92be
AmirHamzahMirza <83139031+AmirHamzahMirza@users.noreply.github.com>
2022-04-22 14:58:02 -0400

    hopefully this fixes it

--- login.php
+++ login.php
@@ -1,8 +1,7 @@
-
 <?php
     if(!isset($_POST["btnLogin"])){
     ?>
-        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+        <form action="login.htm" method="post">
             <input type="text" name="username" value="username">
             <input type="text" name="password" value="password">
             <input type="submit" name="btnLogin" value="Submit">
@@ -18,21 +17,7 @@
             $sql = "SELECT * FROM user WHERE username LIKE ? AND password LIKE ?";
             $stmt = $connect->prepare($sql);
             $stmt->execute([$username, $password]);
-
-            foreach ($stmt->fetchAll() as $k => $row) {
-                ?>
-                <div style="border:1px solid #333; background-color:#fbd2d7; border-radius:5px; padding:16px; align=center">
-                    <h4><?php echo $row["firstname"]; ?></h4>
-                    <h4><?php echo $row["lastname"]; ?></h4>
-                    <h4><?php echo $row["username"]; ?></h4>
-                    <h4><?php echo $row["password"]; ?></h4>
-                    <h4><?php echo $row["email"]; ?></h4>   
-                </div>
-            <?php
-            }
-        } catch (PDOException $e) {
-            echo $sql . "<br>" .$e->getMessage();
-        }
+            
         $connect=null;
     }
 ?>
